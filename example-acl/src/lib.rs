c88a42174aa6631f05ed2d6bc1a07bdc2a723548
Kristoffer Ödmark <kristoffer.odmark90@gmail.com>
2020-12-03 13:22:38 +0100

    added some explanatory text

--- example-acl/src/lib.rs
+++ example-acl/src/lib.rs
@@ -1,14 +1,19 @@
+// Has to be included, to get the errors and success parameters that are used in the
+// generate_dynamic_library macro invocation
 use mosquitto_plugin::*;
 
+// Some simple nonsense structure to use as an example
 #[derive(Debug)]
 pub struct Test {
     i: i32,
     s: String,
 }
 
+// Required trait implementation
 impl MosquittoPlugin for Test {
     fn init(opts: std::collections::HashMap<&str, &str>) -> Self {
         // These are the strings provided after "auth_opt_<key> value" in the mosquitto.conf
+        // only that they are provided on a hashmap form here
         let default = "hej";
         let topic = opts.get("topic").unwrap_or(&default);
         let level = opts.get("level").unwrap_or(&default);
@@ -19,8 +24,8 @@ impl MosquittoPlugin for Test {
             s: topic.to_string(),
         }
     }
-
     fn username_password(&mut self, u: &str, p: &str) -> Result<Success, Error> {
+        // this will allow all username/password where the password is the username in reverse
         let rp: String = p.chars().rev().collect();
         if rp == u {
             Ok(Success)
@@ -36,6 +41,11 @@ impl MosquittoPlugin for Test {
     ) -> Result<Success, mosquitto_plugin::Error> {
         println!("allowed topic: {}", self.s);
         println!("topic: {}", msg.topic);
+        println!("level requested: {}", level);
+
+        // only the topic provided in the mosquitto.conf by the value auth_opt_topic <value> is
+        // allowed, errors will not be reported to the clients though, they will only not be able
+        // to send/receive messages and thus silently fail due to limitations in MQTT protocol
         if msg.topic == self.s {
             Ok(Success)
         } else {
