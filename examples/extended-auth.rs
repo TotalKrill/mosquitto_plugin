bfa8682a412e18909896dc726ba21140b61fc205
Felix Obenhuber <felix@obenhuber.de>
2022-08-22 16:40:12 +0200

    Remove authenticated hash map
    Any client is disconnected when `on_auth_start` or `on_auth_continue`
    return something different that `Ok(Success)` or
    `Err(Error::AuthContinue(..))`. This make the handling of authenticated
    client obsolete.

--- examples/extended-auth.rs
+++ examples/extended-auth.rs
@@ -1,7 +1,7 @@
 #![allow(clippy::not_unsafe_ptr_arg_deref)]
 
 use mosquitto_plugin::*;
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 
 /// Random auth data that is expected from the client.
 const HELLO_BROKER: &str = "hello broker";
@@ -11,10 +11,7 @@ const HELLO_CLIENT: &str = "hello client";
 /// Example plugin for extended auth.
 #[derive(Debug, Default)]
 #[allow(dead_code)]
-pub struct AuthPlugin {
-    /// Set of "authenticated" client ids.
-    authenticated: HashSet<String>,
-}
+pub struct AuthPlugin;
 
 // Required trait implementation
 impl MosquittoPlugin for AuthPlugin {
@@ -27,26 +24,6 @@ impl MosquittoPlugin for AuthPlugin {
             "Plugin on_disconnect, Client {:?} is disconnecting",
             client.get_id()
         );
-
-        if let Some(ref client_id) = client.get_id() {
-            self.authenticated.remove(client_id);
-        }
-    }
-
-    fn acl_check(
-        &mut self,
-        client: &dyn MosquittoClientContext,
-        _acl: AclCheckAccessLevel,
-        _msg: MosquittoMessage,
-    ) -> Result<Success, Error> {
-        println!("Plugin on_acl_check, Client {:?}", client.get_id());
-        let client_id = client.get_id().ok_or(Error::Inval)?;
-
-        if self.authenticated.contains(&client_id) {
-            Ok(Success)
-        } else {
-            Err(Error::Auth)
-        }
     }
 
     /// Authentication start.
@@ -86,12 +63,11 @@ impl MosquittoPlugin for AuthPlugin {
                     "Plugin on_auth_continue, Client {} authenticated",
                     client_id
                 );
-                self.authenticated.insert(client_id.into());
                 Ok(Success)
             } else {
                 println!(
-                    "Plugin on_auth_continue, Client {} failed to authenticate. Expected \"hello\"",
-                    client_id
+                    "Plugin on_auth_continue, Client {} failed to authenticate. Expected \"{}\"",
+                    client_id, HELLO_BROKER,
                 );
                 Err(Error::AuthContinue(HELLO_CLIENT.as_bytes().to_vec()))
             }
